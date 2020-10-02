867fffd6b0d8560d9a13e6b048ae384af01f02dd
Alex Verbruggen <verbruggenalex@gmail.com>
2020-10-02 12:10:00 +0000

    Update twigcs

--- src/Robo/Plugin/Commands/GrumphpStackCommands.php
+++ src/Robo/Plugin/Commands/GrumphpStackCommands.php
@@ -83,7 +83,7 @@ class GrumphpStackCommands extends \Robo\Tasks
 
             $packages = array_keys($suggests);
             // Change version and package names:
-            $packages[] = 'friendsoftwig/twigcs:^4.0@beta';
+            $packages[] = 'friendsoftwig/twigcs:^4.0';
             $packages[] = 'squizlabs/php_codesniffer:3.x-dev';
             $packages[] = 'symplify/easy-coding-standard';
             $packages[] = 'consolidation/robo';
